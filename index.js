9f7a14db8e28f5a28d6b08a3031689bd35f10107
RyanLin0819 <412411786@o365.tku.edu.tw>
2025-03-28 10:25:43 +0800

    3rd commit

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ function openiframe() {
     console.log(c);
     switch(index){
         case 1:
-            c.innerHTML = `<iframe src="w01/w22.html"  frameborder="1"></iframe>`;
+            c.innerHTML = `<iframe src="w1/HW1.html"  frameborder="1"></iframe>`;
             break;
         case 2:
-            c.innerHTML = `<iframe src="0314-main/card_xx.html"  frameborder="1"></iframe>`;
+            c.innerHTML = `<iframe src="0314-main/card.html"  frameborder="1"></iframe>`;
             break;
         case 3:
-            c.innerHTML = `<iframe src="0314-main/blog.html"  frameborder="1"></iframe>`;
+            c.innerHTML = `<iframe src="0314-main/temp.html"  frameborder="1"></iframe>`;
             break;
         case 4:
             c.innerHTML = `<iframe src="class/t3-3.html"  frameborder="1"></iframe>`;
